6e67df79542d4b4e73c03a899926a7c6387a39e4
ragam <ragam@DESKTOP-8I1FF0G>
2024-07-01 15:26:02 +0530

    Ifelse project

---
+++ javaProgram/src/daytwo/IfElseDemo.java
@@ -0,0 +1,15 @@
+package daytwo;
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age= 11;
+		if(age>=18)
+			System.out.println("you are eligible for Voting");
+		else
+			System.out.println("Sorry! you need towait more");
+		System.out.println("if else demonstration");
+	}
+
+}
